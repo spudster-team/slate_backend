0b339650a5eb2c3cc7025b3a5f9581aba4818850
j03-dev <24nomeniavo@gmail.com>
2023-11-09 11:42:40 +0300

    add static file on this project

--- slate/settings.py
+++ slate/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = path.join(BASE_DIR, "static")
+STATIC_ROOT = path.join(BASE_DIR, "static")
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
