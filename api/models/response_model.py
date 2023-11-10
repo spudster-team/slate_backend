6d2f819d459d4c432557b478ef9b2d110be3e5f5
j03-dev <24nomeniavo@gmail.com>
2023-11-10 14:52:08 +0300

    remove limite on content

--- api/models/response_model.py
+++ api/models/response_model.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Response(models.Model):
     owner = models.ForeignKey("User", on_delete=models.CASCADE)
-    content = models.TextField(max_length=255)
+    content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     photo = models.ForeignKey("Photo", on_delete=models.CASCADE, null=True, blank=True)
     vote = models.ManyToManyField("Vote", blank=True)
